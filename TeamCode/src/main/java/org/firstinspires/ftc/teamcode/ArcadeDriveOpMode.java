e72f39c82c9066034a23ea2d91b35f68f5739efa
ftc22918 <ftc.22918@gmail.com>
2026-04-11 11:40:59 -0400

    initial changes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArcadeDriveOpMode.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.mechanisms.ArcadeDrive;
+
+@Disabled
+@TeleOp
+public class ArcadeDriveOpMode extends OpMode {
+
+    ArcadeDrive drive = new ArcadeDrive();
+
+    double throttle, spin;
+
+    @Override
+    public void init() {
+        drive.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        throttle = -gamepad1.left_stick_y;
+        spin = gamepad1.right_stick_x;
+        drive.drive(throttle, spin);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DcMotorPractice.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.mechanisms.DecodeHardwareSetup;
+
+@TeleOp
+public class DcMotorPractice extends OpMode {
+
+    DecodeHardwareSetup robot = new DecodeHardwareSetup();
+
+    @Override
+    public void init() {
+        robot.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        double motorSpeed = gamepad1.left_stick_y;
+        robot.setMotorSpeed(motorSpeed);
+    }
+}
