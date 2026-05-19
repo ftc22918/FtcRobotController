b4c7190bc017b292e17a84cd8c91e8e205a442c4
ftc22918 <ftc.22918@gmail.com>
2026-05-19 19:31:39 -0400

    Changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/brogan_training/ArcadeDriveOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/brogan_training/ArcadeDriveOpMode.java
@@ -1,9 +1,8 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.brogan_training;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.mechanisms.ArcadeDrive;
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/brogan_training/RobotLocationPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/brogan_training/RobotLocationPractice.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.brogan_training;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
