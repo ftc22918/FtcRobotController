dd3caaec456e26118a113efb5efe0c9dae4bdadb
ftc22918 <ftc.22918@gmail.com>
2026-05-21 18:39:30 -0400

    update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/newrobot/Main_TeleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/newrobot/Main_TeleOP.java
@@ -27,7 +27,16 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp(name="_Main")
 public class Main_TeleOP extends LinearOpMode {
 
+    double motorSensitivity = 1.0;
+    double initMotorPower = 0;
+
+
     DcMotor motorLeftForward;
+    DcMotor motorLeftRear;
+    DcMotor motorRightForward;
+    DcMotor motorRightRear;
+
+
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -37,17 +46,58 @@ public class Main_TeleOP extends LinearOpMode {
         }
         waitForStart();
         while (opModeIsActive()) {
+            teleOpControls();
             opModeTelemetry();
         }
     }
     public void initHardware() {
-        initMotorLeftFront();
+        initMotorLeftForward();
+        initMotorLeftRear();
+        initMotorRightForward();
+        initMotorRightRear();
     }
 
-    private void initMotorLeftFront() {
+    private void initMotorLeftForward() {
         motorLeftForward = hardwareMap.get(DcMotor.class, "motor_left_forward");
+        motorLeftForward.setPower(initMotorPower);
         motorLeftForward.setDirection(DcMotor.Direction.FORWARD);
+        motorLeftForward.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorLeftForward.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorLeftForward.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
 
+    private void initMotorLeftRear() {
+        motorLeftRear = hardwareMap.get(DcMotor.class, "motor_left_rear");
+        motorLeftRear.setPower(initMotorPower);
+        motorLeftRear.setDirection(DcMotor.Direction.FORWARD);
+        motorLeftRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorLeftRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorLeftRear.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    private void initMotorRightForward() {
+        motorRightForward = hardwareMap.get(DcMotor.class, "motor_right_forward");
+        motorRightForward.setPower(initMotorPower);
+        motorRightForward.setDirection(DcMotor.Direction.FORWARD);
+        motorRightForward.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorRightForward.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorRightForward.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    private void initMotorRightRear() {
+        motorRightRear = hardwareMap.get(DcMotor.class, "motor_right_rear");
+        motorRightRear.setPower(initMotorPower);
+        motorRightRear.setDirection(DcMotor.Direction.FORWARD);
+        motorRightRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorRightRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorRightRear.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    public void teleOpControls() {
+        motorLeftForward.setPower(gamepad1.left_stick_x * motorSensitivity);
+        motorLeftRear.setPower(gamepad1.left_stick_x * motorSensitivity);
+        motorRightForward.setPower(gamepad1.left_stick_x * motorSensitivity);
+        motorRightRear.setPower(gamepad1.left_stick_x * motorSensitivity);
     }
 
     private void opModeTelemetry() {
@@ -59,6 +109,7 @@ public class Main_TeleOP extends LinearOpMode {
         telemetry.addData("mlf manufacture: ", motorLeftForward.getManufacturer());
         telemetry.addData("mlf version: ", motorLeftForward.getVersion());
         telemetry.addData("mlf class: ", motorLeftForward.getClass());
+        telemetry.addData("mlf mode: ", motorLeftForward.getMode());
         telemetry.update();
     }
 }
